b4309ef1dadea30afc5e271db859c60f747f8c5b
Dakshi <dakshiegoel@gmail.com>
2023-03-29 00:16:20 +0530

    updated homepage

--- main_ui.py
+++ main_ui.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1 style='color:purple'>HELLO WORLD</h1>"
+    return render_template("homepage.html")
 
 @app.route("/about")
 def about():
