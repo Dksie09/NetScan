04abf730e42b182c5ef7641e46251bb2f2db0207
Dakshi <dakshiegoel@gmail.com>
2023-03-31 16:53:34 +0530

    updated files

--- main_ui.py
+++ main_ui.py
@@ -6,9 +6,6 @@ app = Flask(__name__)
 def home():
     return render_template("homepage.html")
 
-@app.route("/about")
-def about():
-    return "<h1 style='color:yellow'>yay</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
