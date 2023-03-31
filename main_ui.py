62cf994cd5db79c36c2b07fd1ce1276635d7c581
Dakshi <dakshiegoel@gmail.com>
2023-03-31 16:59:14 +0530

    updated files

--- main_ui.py
+++ main_ui.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("homepage.html")
+
+@app.route("/get_ip", methods=["POST"])
+def get_ip():
+    ip = request.form.get('ip-address')
+    return "Your IP is: " + ip
 
 
 if __name__ == "__main__":
